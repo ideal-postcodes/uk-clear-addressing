da1be4904d4c7827ac80c0e77a93ab5539ec335f
Chris Blanchard <cablanchard@gmail.com>
2018-07-02 13:07:25 +0100

    Fix sorting algorithm

--- src/sort.ts
+++ src/sort.ts
@@ -1,64 +1,9 @@
 import { Address } from "./index";
 import { nameException } from "./rules";
-import * as t from "./types";
+import { SortingElems } from "./types";
+import { alphanum } from "@cablanchard/koelle-sort";
 
-/**
- * Returns true if charCode is '.' or numeric
- */
-const isNumeric = (i: number): boolean => i === 46 || (i >= 48 && i <= 57);
-
-/**
- * Breaks string into an array of continuous number or alphabetical strings
- */
-const chunkify = (t: string): string[] => {
-	let result: string[] = [];
-	let x = 0;
-	let y = -1;
-	let n = false;
-	let i; // Tracks charCode
-	let j; // Tracks character
-
-	i = (j = t.charAt(x++)).charCodeAt(0);
-	while (i) {
-		let m = isNumeric(i);
-		if (m !== n) {
-			result[++y] = "";
-			n = m;
-		}
-		result[y] += j;
-		x += 1;
-		i = (j = t.charAt(x++)).charCodeAt(0);
-	}
-	return result;
-};
-
-/**
- * Sorts by number if strings numeric, otherwise performs standard string
- * comparison
- *
- * Based on the Alphanum Algorithm by Brian Huisman (http://www.davekoelle.com/files/alphanum.js)
- */
-const alphaNumSort = (a: string, b: string): number => {
-	const aa = chunkify(a.toLowerCase());
-	const bb = chunkify(b.toLowerCase());
-
-	for (let x = 0; aa[x] && bb[x]; x++) {
-		if (aa[x] !== bb[x]) {
-			const c = Number(aa[x]);
-			const d = Number(bb[x]);
-			if (c.toString() === aa[x] && b.toString() === bb[x]) {
-				// Exit: Both strings numeric
-				return c - d;
-			} else {
-				// Exit: compare strings
-				return (aa[x] > bb[x]) ? 1 : -1;
-			}
-		}
-	}
-	return aa.length - bb.length;
-};
-
-const sortingElems: t.SortingElems[] = [
+const sortingElems: ReadonlyArray<SortingElems> = [
 	"building_number",
 	"building_name",
 	"sub_building_name",
@@ -82,17 +27,17 @@ const extractIntegerAttribute = (a: Address): string => {
  */
 export const sort = (a: Address, b: Address): number => {
 	for (let i = 0; i < sortingElems.length; i++) {
-		let addressAttribute: t.SortingElems = sortingElems[i];
-		let elemA, elemB;
-		if (addressAttribute === "building_number") {
-			elemA = extractIntegerAttribute(a);
-			elemB = extractIntegerAttribute(b);
+		let attr: SortingElems = sortingElems[i];
+		let A, B: string;
+		if (attr === "building_number") {
+			A = extractIntegerAttribute(a);
+			B = extractIntegerAttribute(b);
 		} else {
-			elemA = a[addressAttribute];
-			elemB = b[addressAttribute];
+			A = a[attr];
+			B = b[attr];
 		}
-		if (elemA === elemB) continue;
-		return alphaNumSort(elemA, elemB);
+		if (A === B) continue;
+		return alphanum(A, B);
 	}
 	return 0;
 };
