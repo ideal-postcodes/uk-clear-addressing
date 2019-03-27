ec2e63eff30c0ddde06e3fd55245542756d98f84
Christopher Blanchard <cablanchard@gmail.com>
2019-03-27 14:24:39 +0000

    Add accessor examples in documentation

--- example.js
+++ example.js
@@ -3,14 +3,11 @@ const { Address } = require("uk-clear-addressing");
 const address = new Address({
   postcode: "WS11 5SB",
   post_town: "CANNOCK",
-  dependant_locality: "",
-  double_dependant_locality: "",
   thoroughfare: "Pye Green Road",
-  building_number: "",
   building_name: "Flower House 189A",
-  sub_building_name: "",
-  dependant_thoroughfare: "",
-  organisation_name: "S D Alcott Florists"
+  organisation_name: "S D Alcott Florists",
 });
 
 console.log(address.formattedAddress());
+
+const { line_1, line_2, line_3, post_town, postcode, premise } = address;
